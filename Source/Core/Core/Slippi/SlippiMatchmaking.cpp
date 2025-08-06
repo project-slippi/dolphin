c1f48c0fd61b243d7582a6ef86156d09fc9a5b8b
Jas Laferriere <Fizzi36@gmail.com>
2025-08-06 14:06:05 -0400

    fix: allow rank element to be fetched correctly

--- Source/Core/Core/Slippi/SlippiMatchmaking.cpp
+++ Source/Core/Core/Slippi/SlippiMatchmaking.cpp
@@ -557,7 +557,7 @@ void SlippiMatchmaking::handleMatchmaking()
         player_info.chat_messages = m_user->GetDefaultChatMessages();
       }
 
-      json rank_el = el.value("rank", "");
+      json rank_el = el["rank"];
       if (rank_el.is_object())
       {
         player_info.ranked_rating = rank_el.value("rating", 0.0);
