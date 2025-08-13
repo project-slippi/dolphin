ea88a9f9a5deb34c654bdd7bdc8938135f9fa00f
Jas Laferriere <Fizzi36@gmail.com>
2025-08-13 14:24:38 -0400

    fix: get playback config building again

--- Source/Core/VideoCommon/OnScreenDisplay.cpp
+++ Source/Core/VideoCommon/OnScreenDisplay.cpp
@@ -279,8 +279,8 @@ bool ButtonCustom(const char* label, const ImVec2& size_arg,
   if (!ImGui::ItemAdd(bb, id))
     return false;
 
-  if (g.CurrentItemFlags & ImGuiItemFlags_ButtonRepeat)
-    flags |= ImGuiButtonFlags_Repeat;
+  //if (g.CurrentItemFlags & ImGuiItemFlags_ButtonRepeat)
+  //  flags |= ImGuiButtonFlags_Repeat;
   bool hovered, held;
   bool pressed = ImGui::ButtonBehavior(bb, id, &hovered, &held, flags);
   if (pressed)
